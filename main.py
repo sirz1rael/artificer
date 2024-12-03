04128231204230d9d078b3b99b05d6ca65646525
sirz1rael <sir.t0gga@gmail.com>
2024-12-04 00:11:42 +0300

    Fully working CMAKE

--- main.py
+++ main.py
@@ -26,18 +26,32 @@ ast_f = sys.argv[1] + "/assets "    # Default assets folder - /assets
 
 mkdir = {lib_f, bin_f, inc_f, src_f, ast_f}
 
+mainfile_cpp_text = '#include<iostream>\n\nint main() {\n\tstd::cout << "Hello, world!" << std::endl;\n\treturn 0;\n}'
+
+def gen_main_file(j:str, i:str):
+
+    mainfile = open(src_f[:-1] + "/" + j + "." + i, "w")
+    mainfile.write(mainfile_cpp_text)
+    mainfile.close()
+
 def cmake():
+    mainfile_name = 'main'
+    mainfile_ext = 'cpp'
+
     for i in mkdir:
         os.system("mkdir " + i)
 
-    cmakelists = open(project_path + "CMakeLists.txt", "w")
+    cmakelists = open(project_path + "/CMakeLists.txt", "w")
 
     project_name = "project(" + str(input("Name your project: ")) + ")"
     cmake_ver = "cmake_minimum_required(VERSION " + str(cmake_ver_coutput[2])[2:5] + ")"
 
-    cmakelists.write(cmake_ver + "\n")
-    cmakelists.write(project_name + "\n")
+    cmakelists.write(cmake_ver + "\n")          # cmake_minimum_required()
+    cmakelists.write(project_name + "\n")       # project(<name>)
 
+    gen_main_file('main', 'cpp')                # generating mainfile
+
+    cmakelists.write("add_executable(test " + str(src_f.split('/')[1])[:-1] + '/' + mainfile_name + '.' + mainfile_ext + ")") # add_executable()
 
     cmakelists.close()
     os.system("cd " + project_path + "&& cmake .")
